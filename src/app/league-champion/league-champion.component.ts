97292fca63d3ba21f4ed49b095beca380ed00c75
Icepick <remi.jara@laposte.net>
2022-10-28 17:16:48 +0200

    Initiate the project with Angular

---
+++ src/app/league-champion/league-champion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-league-champion',
+    templateUrl: './league-champion.component.html',
+    styleUrls: ['./league-champion.component.scss'],
+})
+export class LeagueChampionComponent {}
