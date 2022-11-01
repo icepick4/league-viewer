4d26ccc81122fa51c4fb9f4ab124a49ff7c72b18
Icepick <remi.jara@laposte.net>
2022-11-01 18:23:19 +0100

    adding files for close component

---
+++ src/app/close-champion/close-champion.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-close-champion',
+    templateUrl: './close-champion.component.html',
+    styleUrls: ['./close-champion.component.scss'],
+})
+export class CloseChampionComponent implements OnInit {
+    type: string = this.router.url.split('/')[2];
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {}
+
+    close(): void {
+        this.router.navigate([`/champions/${this.type}`]);
+    }
+}
