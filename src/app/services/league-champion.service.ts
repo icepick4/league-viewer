c1999b9e6953df2aecbb8d63ad82e96f36a32ed2
icepick4 <remi.jara@laposte.net>
2022-11-09 10:27:13 +0100

    save languages once you charged them

--- src/app/services/league-champion.service.ts
+++ src/app/services/league-champion.service.ts
@@ -1,42 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Champion } from '../models/champion.model';
+import { HashTable } from '../models/hashTable.model';
 import { Language } from '../models/language.model';
 @Injectable({
     providedIn: 'root',
 })
 export class LeagueChampionService {
-    champions: Champion[];
+    champions!: HashTable<Champion[]>;
     languages: Language[];
     language: Language = { name: 'English', code: 'en_US', charged: true };
     purcentage: number = 0;
+    version!: string;
     constructor(private router: Router) {
-        this.champions = [];
         this.languages = [];
-        this.fetchAllLanguages();
-        this.fetchAllChampions(this.language.code);
+        this.champions = {};
+        //wait for getVerison to finish
+        this.getVersion().then(() => {
+            this.fetchAllLanguages();
+            this.fetchAllChampions(this.language.code);
+        });
     }
 
-    async fetchAllChampions(language: string): Promise<void> {
-        console.log('fetchAllChampions');
-        this.champions = [];
-        let temp_champions = [];
+    async getVersion(): Promise<void> {
         const version_res = await fetch(
             'http://ddragon.leagueoflegends.com/api/versions.json'
         );
         const version = (await version_res.json())[0];
+        this.version = version;
+    }
+
+    async fetchAllChampions(language: string): Promise<void> {
+        console.log('fetchAllChampions');
+        let temp_champions: Champion[] = [];
         const global_res = await fetch(
-            `http://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`
+            `http://ddragon.leagueoflegends.com/cdn/${this.version}/data/${language}/champion.json`
         );
         const data = await global_res.json();
         const fetched_champions = data.data;
         let i = 0;
         for (const champion in fetched_champions) {
+            console.log(i);
             this.purcentage = Math.round(
                 (i / Object.keys(fetched_champions).length) * 100
             );
             const champ_res = await fetch(
-                `http://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion/${fetched_champions[champion].id}.json`
+                `http://ddragon.leagueoflegends.com/cdn/${this.version}/data/${language}/champion/${fetched_champions[champion].id}.json`
             );
             const champ_data = await champ_res.json();
             const champ = champ_data.data[fetched_champions[champion].id];
@@ -63,7 +72,7 @@ export class LeagueChampionService {
                 champ.id +
                 '_0.jpg';
             const icon =
-                `http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/` +
+                `http://ddragon.leagueoflegends.com/cdn/${this.version}/img/champion/` +
                 champ.id +
                 '.png';
             let championObj: Champion = new Champion(
@@ -81,7 +90,7 @@ export class LeagueChampionService {
         }
         this.router.navigateByUrl('/');
         this.language.charged = true;
-        this.champions = temp_champions;
+        this.champions[language] = temp_champions;
     }
 
     async fetchAllLanguages(): Promise<void> {
@@ -102,6 +111,7 @@ export class LeagueChampionService {
                 };
                 if (languageObject.code != 'id_ID') {
                     this.languages.push(languageObject);
+                    console.log(languageObject.code);
                 }
             }
         }
@@ -113,10 +123,14 @@ export class LeagueChampionService {
 
     changeLanguage(language: string): void {
         console.log(language);
-        this.fetchAllChampions(language);
         this.language = this.languages.find(
             (lang) => lang.code == language
         ) as Language;
+        if (this.champions[language] == undefined) {
+            this.fetchAllChampions(this.language.code);
+        } else {
+            this.language.charged = true;
+        }
     }
 
     changeSkinRight(champion: Champion): void {
@@ -136,15 +150,15 @@ export class LeagueChampionService {
     }
 
     getChampionByName(name: string): Champion | null {
-        for (let champion in this.champions) {
-            if (this.champions[champion].name == name) {
-                return this.champions[champion];
+        for (let champion in this.champions[this.language.code]) {
+            if (this.champions[this.language.code][champion].name == name) {
+                return this.champions[this.language.code][champion];
             }
         }
         return null;
     }
 
     getAllChampions(): Champion[] {
-        return this.champions;
+        return this.champions[this.language.code];
     }
 }
