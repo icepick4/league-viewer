6393c12dc8084b69a99bc2f101dee6154c432db6
Icepick <remi.jara@laposte.net>
2022-11-11 17:16:25 +0100

    right language name on loading component

--- src/app/league-champion-list/league-champion-list.component.ts
+++ src/app/league-champion-list/league-champion-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Champion } from '../models/champion.model';
+import { languagesNames } from '../models/language.model';
+
 import { LeagueChampionService } from '../services/league-champion.service';
 @Component({
     selector: 'app-league-champion-list',
@@ -23,5 +25,9 @@ export class LeagueChampionListComponent implements OnInit {
         //get the param type in url
         const type = this.activatedRoute.snapshot.params['type'];
         this.router.navigate([`/champions/${type}/${name}`]);
+    }
+
+    getLanguageName() {
+        return languagesNames[this.leagueChampionService.language.code];
     }
 }
