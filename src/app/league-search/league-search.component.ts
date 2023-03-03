f206ff401a7c72aff8abab90248e14ce3e7d5dc9
icepick4 <remi.jara@laposte.net>
2023-03-03 13:03:17 +0100

    optimize filtering + fix issue on unfilter

--- src/app/league-search/league-search.component.ts
+++ src/app/league-search/league-search.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { roles } from '../models/champion.model';
 import { LeagueChampionService } from '../services/league-champion.service';
 @Component({
@@ -11,42 +10,33 @@ export class LeagueSearchComponent implements OnInit {
     search!: string;
     roles = roles;
     selectedRole!: string;
-    constructor(
-        private leagueChampionService: LeagueChampionService,
-        private router: Router,
-        private route : ActivatedRoute
-    ) {
-        router.events.subscribe(async () => {
-            if (router.url === '/') {
-                this.leagueChampionService.unfilterChampions();
-            }
-        });
-    }
+    constructor(private leagueChampionService: LeagueChampionService) {}
 
     ngOnInit(): void {
         this.selectedRole = this.leagueChampionService.role;
         this.search = this.leagueChampionService.search;
-        const input = document.getElementById('input-champ') as HTMLInputElement;
+        const input = document.getElementById(
+            'input-champ'
+        ) as HTMLInputElement;
         input.value = this.search;
     }
 
     filterChampions(event: any): void {
-        console.log("filterChampions")
+        console.log('filterChampions');
         this.search = event.target.value;
         const filter = event.target.value;
         //remove all spaces
         const filterNoSpaces = filter.replace(/\s/g, '');
-        this.leagueChampionService.filterChampionsByName(filterNoSpaces);
+        this.leagueChampionService.setSearch(filterNoSpaces);
     }
 
     filterChampionsByRole(role: string): void {
-        if (role != this.selectedRole){
+        if (role != this.selectedRole) {
             this.selectedRole = role;
-            this.leagueChampionService.filterChampionsByRole(this.selectedRole);
-        }
-        else{
+            this.leagueChampionService.setRole(this.selectedRole);
+        } else {
             this.selectedRole = '';
-            this.leagueChampionService.unfilterChampions();
+            this.leagueChampionService.resetRole();
         }
     }
 }
